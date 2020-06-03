18cfcd33e6f1706a6f6b96e2a2d082d4cc341f51
Scott Reilly <scott@coffee2code.com>
2020-06-02 23:59:25 -0700

    Add TODO.md and move existing TODO list from top of main plugin file into it

--- admin-commenters-comments-count.php
+++ admin-commenters-comments-count.php
@@ -22,19 +22,6 @@
  */
 
 /*
- * TODO:
- * - When a comment gets approved/unapproved via comment action links, update commenter's count accordingly
- * - Allow admin to manually group commenters with different email addresses (allows grouping a person who
- *   may be using multiple email addresses, or maybe admin prefers to group people per organization). The reported
- *   counts would be for the group and not the individual. The link to see the emails would search for all of the
- *   email addresses in the group. Via filter maybe?
- * - Add sortability to 'Comments' column in user table
- * - Consider inserting commenter bomment bubble via 'comment_row_actions' hook like Akismet does, though that
- *   requires introducing a JS dependency.
- *
- */
-
-/*
 	Copyright (c) 2009-2020 by Scott Reilly (aka coffee2code)
 
 	This program is free software; you can redistribute it and/or
